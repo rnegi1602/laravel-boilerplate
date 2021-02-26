3c67ccc66d970d8dc7ab9cbe57c64a16fbd1a6ac
Renuka Negi <renuka@rwit.io>
2021-02-26 13:59:20 +0530

    Initial setup of admin-lte theme

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{ asset('js/plugins/fontawesome-free/css/all.min.css') }}">
+    <!-- Tempusdominus Bootstrap 4 -->
+    {{-- <link rel="stylesheet" href="{{ asset('js/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css') }}"> --}}
+    <!-- iCheck -->
+    <link rel="stylesheet" href="{{ asset('js/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{ asset('js/dist/css/adminlte.min.css') }}">
+</head>
+
+<body class="hold-transition sidebar-mini layout-fixed">
+    <div id="app" class="wrapper">
+        @include('partials.navbar')
+        @include('partials.sidebar')
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+    <!-- jQuery UI 1.11.4 -->
+    <script src="{{ asset('js/plugins/jquery-ui/jquery-ui.min.js') }}" defer></script>
+    <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+    {{-- <script >
+        $.widget.bridge('uibutton', $.ui.button)
+    </script> --}}
+    <!-- Tempusdominus Bootstrap 4 -->
+    {{-- <script src="{{ asset('js/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js') }}" defer></script> --}}
+    <!-- AdminLTE App -->
+    <script src="{{ asset('js/dist/js/adminlte.min.js') }}" defer></script>
+</body>
+
+</html>
